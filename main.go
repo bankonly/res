eacd6c2783a806b64b7536eb06ab9c9bcc4785ff
bankonly <for21bn01@gmail.com>
2021-11-16 23:54:40 +0700

    update

--- main.go
+++ main.go
@@ -18,28 +18,28 @@ func BadRequest(c *gin.Context, errCode string) {
 	c.AbortWithStatusJSON(r.responseCode, gin.H{"message": r.message, "errCode": r.errCode})
 }
 
-func Unauthorized(c *gin.Context) {
+func Unauthorized(c *gin.Context, message string) {
 	var r R
 	r.errCode = "ERR419"
 	r.message = "Unauthorized"
 	r.responseCode = 419
-	c.AbortWithStatusJSON(r.responseCode, gin.H{"message": r.message, "errCode": r.errCode})
+	c.AbortWithStatusJSON(r.responseCode, gin.H{"message": r.message, "errCode": r.errCode, "errMessage": message})
 }
 
-func InternalError(c *gin.Context) {
+func InternalError(c *gin.Context, message string) {
 	var r R
 	r.errCode = "ERR500"
 	r.message = "Something went wrong"
 	r.responseCode = 500
-	c.AbortWithStatusJSON(r.responseCode, gin.H{"message": r.message, "errCode": r.errCode})
+	c.AbortWithStatusJSON(r.responseCode, gin.H{"message": r.message, "errCode": r.errCode, "errMessage": message})
 }
 
-func AccessDenied(c *gin.Context) {
+func AccessDenied(c *gin.Context, message string) {
 	var r R
 	r.errCode = "ERR405"
 	r.message = "Access denied"
 	r.responseCode = 500
-	c.AbortWithStatusJSON(r.responseCode, gin.H{"message": r.message, "errCode": r.errCode})
+	c.AbortWithStatusJSON(r.responseCode, gin.H{"message": r.message, "errCode": r.errCode, "errMessage": message})
 }
 
 func ToJson(c *gin.Context, h gin.H) {
